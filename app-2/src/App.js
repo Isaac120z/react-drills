1927335fa0530d857071fc3ad77d8731706ad395
Isaac <Isaac120z@msn.com>
2018-03-20 21:50:06 -0500

    Completed Projects 1 and 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+
+    this.state = {
+      bands:["Pink Floyd","The Who", "The Clash", "Rolling Stones", "The Beatles"]
+    }
+  }
+
+
   render() {
+    let ListOfBands = this.state.bands.map((e,i)=>{
+      return (
+      <h1 key={i}>{e}
+      </h1>)
+    })
+
+
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {ListOfBands}
+    
       </div>
     );
   }
